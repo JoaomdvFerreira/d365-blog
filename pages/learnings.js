650237b8ca7442fa6955229d31b298f7afd8997e
joao.ferreira <joao.ferreira@savills.com>
2023-02-25 17:07:22 +0000

    first commit

---
+++ pages/learnings.js
@@ -0,0 +1,23 @@
+export default function Learnings() {
+  return (
+    <div>
+      <div>
+        <h2 className="text-xl font-semibold">Learnings journal</h2>
+        <span className="font-semibold">Doings and learnings.</span> <br />
+      </div>
+      <div className="mt-3 mb-3">
+        <h5>15 - 21 August</h5>
+        <ul>
+          <li>React router v4.6 - https://beta.reactrouter.com/en/dev/route/route</li>
+          <li>yerked-dodson law - https://en.wikipedia.org/wiki/Yerkes%E2%80%93Dodson_law</li>
+          <li>
+            the paradox of effort -
+            https://www.scotthyoung.com/blog/2020/11/09/the-paradox-of-effort/
+          </li>
+          <li>writing well - https://www.julian.com/guide/write/intro</li>
+          <li>edgeDb - https://www.edgedb.com/</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
