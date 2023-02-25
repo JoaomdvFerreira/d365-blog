b73d868e14e537e0b39800b008df19e139e75a12
joao.ferreira <joao.ferreira@savills.com>
2023-02-25 19:49:29 +0000

    v1.1 completed

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,11 +1,23 @@
 const projectsData = [
   {
-    title: 'Testing Title',
-    description: `Testing Description`,
+    title: 'Rock Paper Scissors',
+    description: `Simple Rock Paper Scissors only for major dorks. 🔥`,
     imgSrc: '',
-    href: 'https://d365blog.vercel.app/',
+    href: 'https://rps-ten.vercel.app/',
     github: 'https://github.com/JoaomdvFerreira/rps',
     tech1: 'JS',
+    tech2: 'HTML',
+    tech3: 'CSS',
+  },
+  {
+    title: 'Speed Typing',
+    description: `Speed Typing game connected with API Donald Trump best quotes. What else? 💩`,
+    imgSrc: '',
+    href: 'https://speed-typing-coral.vercel.app/',
+    github: 'https://github.com/JoaomdvFerreira/SpeedTyping',
+    tech1: 'JS',
+    tech2: 'HTML',
+    tech3: 'CSS',
   },
 ]
 
